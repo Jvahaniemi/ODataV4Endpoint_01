6324c3a0471e5b58534233d111cd0dea69214b8f
unknown <k1201190@CA440411.epedu.local>
2015-03-26 18:41:10 +0200

    Client eka osio

---
+++ ODataV4Endpoint/Models/ProductsContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.Data.Entity;
+
+namespace ODataV4Endpoint.Models
+{
+    public class ProductsContext : DbContext
+    {
+        // Web.config connection string
+        public ProductsContext()
+            : base("name=ProductsContext")
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
